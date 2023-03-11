ed81c86f39181c52c237c6d2e89d9c4e8b7cb970
danii <danii0110@naver.com>
2023-03-11 11:32:36 +0900

    20230311 11:32
    수정
    src>>chapter_04>>Clock 오류나서 그냥 지워버림

--- first_react_practice/src/index.js
+++ first_react_practice/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import Library from './chapter_03/Library';
-import Clock from './chapter_04/Clock';
 import CommentList from './chapter_05/CommentList';
 import NotificationList from './chapter_06/NotificationList';
 
